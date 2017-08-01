0251a0123b869960c06d7efe08cddd53670148e7
Roy Segall <roy.segall@gizra.com>
2017-08-01 23:16:05 +0300

    Pushing a grid :)

--- src/elm/Components/Items.elm
+++ src/elm/Components/Items.elm
@@ -26,14 +26,15 @@ getAllItems =
     pizza = Item "sdasd2334431221" "Pizza" 10.45 "https://sep.yimg.com/ay/yhst-19802326331255/pizza-slice-pennant-3x5-9.jpg"
     coke = Item "3343dcc334" "Coca Cola" 8.8 "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flasche_Coca-Cola_0%2C2_Liter.jpg/220px-Flasche_Coca-Cola_0%2C2_Liter.jpg"
 
-    listOfItems = [pizza, pizza, pizza, coke]
+    allItems = [pizza, pizza, pizza, pizza, coke]
+    -- Find a way to split all the array into chunks of 4.
+    listOfItems = [[pizza, pizza, pizza, pizza], [coke]]
 
   in
-    div [ class "row" ]
-    [
-      ul []
-        (List.map (\l -> li [] [ singleItem l True ]) listOfItems)
-    ]
+    div []
+      (List.map (\items -> div [ class "row" ]
+        (List.map (\item -> div [class "col-md-3" ] [(singleItem item True )] ) items)
+       ) listOfItems)
 
 -- Single item display
 singleItem : Item -> Bool -> Html a
@@ -48,13 +49,13 @@ singleItem item showAddToCart =
           ]
         ]
       else
-        Html.text "B"
+        Html.text ""
   in
     div [ class "item" ]
     [
       div [ class "row" ] [
-        div [ class "col-md-1" ] [ img [ src item.image, class "img-responsive" ] [] ]
-        , div [ class "col-md-11" ] [
+        div [ class "col-md-6" ] [ img [ src item.image, class "img-responsive" ] [] ]
+        , div [ class "col-md-6" ] [
           div [] [ text item.title ]
           , div [] [ text ( toString item.price ) ]
         ]
