58cb82893236d25be71cd37c1c3b81a441959718
Roy Segall <roy.segall@gizra.com>
2017-09-01 14:51:57 +0300

    Down with the routing.

--- src/elm/Views/NotFound.elm
+++ src/elm/Views/NotFound.elm
@@ -10,8 +10,16 @@ import Update exposing (..)
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ section []
-            [ text "Bad page!"
+    section [ class "not-found" ]
+        [ div [ class "first" ] [ img [ src "static/img/storm.png" ] [] ]
+        , div [ class "second" ]
+            [ h1 [] [ text "This is not the page you are looking for" ]
+            , p []
+                [ text
+                    ("It's look that force has brought in bad way to this page."
+                        ++ "Don't be afraid of that. Fear is the path to the dark side. "
+                        ++ "Fear leads to anger. Anger leads to hate. Hate leads to suffering."
+                    )
+                ]
             ]
         ]
