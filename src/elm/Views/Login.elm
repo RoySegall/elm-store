2791a9c2f7a4b59b5714bbb0f26e0d9dde39166d
Roy Segall <roy.segall@gizra.com>
2017-09-01 16:31:40 +0300

    Start working on the login.

--- src/elm/Views/Login.elm
+++ src/elm/Views/Login.elm
@@ -9,8 +9,18 @@ import Update exposing (..)
 
 view : Model -> Html Msg
 view model =
-    div [ class "content" ]
-        [ section []
-            [ text "a"
+    section [ class "content login" ]
+        [ Html.form []
+            [ h1 [] [ text "Login" ]
+            , p [] [ span [] [ text "Forgot your password? Why not " ], a [ href "" ] [ text "reset" ], span [] [ text " your password?" ] ]
+            , div [ class "input-group" ]
+                [ span [ class "input-group-addon", id "basic-addon1" ] [ i [ class "fa fa-user" ] [] ]
+                , input [ type_ "text", class "form-control", placeholder "Username" ] []
+                ]
+            , div [ class "input-group" ]
+                [ span [ class "input-group-addon", id "basic-addon1" ] [ i [ class "fa fa-key" ] [] ]
+                , input [ type_ "password", class "form-control", placeholder "" ] []
+                ]
+            , div [ class "actions" ] [ button [ class "btn btn-success" ] [ text "login" ] ]
             ]
         ]
