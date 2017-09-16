b7e5851b70c9df03c699720ca19c9bc37a62e4b1
Roy Segall <roy.segall@gizra.com>
2017-09-16 21:41:38 +0300

    Adding the cart in the profile.

--- src/elm/Views/Profile.elm
+++ src/elm/Views/Profile.elm
@@ -1,15 +1,35 @@
 module Views.Profile exposing (..)
 
+import Components.Items exposing (currentItems, singleItem)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import List exposing (length)
 import Model exposing (..)
 
 
 view : Model -> Html Msg
 view model =
+    let
+        items =
+            if length model.cartItems == 0 then
+                div
+                    []
+                    [ div [ class "no-items" ] [ text "Whoops... Your cart is empty." ]
+                    ]
+            else
+                div [ class "items-list" ]
+                    (List.map (\item -> div [] [ singleItem model item False ]) model.cartItems)
+    in
     div [ class "content profile" ]
         [ section [ onClick HideCart ]
-            [ text "a"
+            [ h1 [] [ text model.loggedUser.username ]
+            , div [ class "row" ]
+                [ div [ class "col-md-6" ] [ img [ src (model.backendAddress ++ "/" ++ model.loggedUser.image), class "img-responsive" ] [] ]
+                , div [ class "col-md-6" ]
+                    [ h4 [] [ text "Your current cart:" ]
+                    , items
+                    ]
+                ]
             ]
         ]
